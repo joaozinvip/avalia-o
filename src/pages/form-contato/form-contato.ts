2c57a01457b0e7f2ac3384afd5188955f48aa141
Daniel Cavalcanti <daniel.cavalcanti@outlook.com.br>
2019-03-25 19:55:07 -0300

    Aula 2, parte 2

--- src/pages/form-contato/form-contato.ts
+++ src/pages/form-contato/form-contato.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -7,16 +8,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'form-contato.html',
 })
 export class FormContatoPage {
+  // 1. declarar e importar
+  formGroup: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder//2. Inserir o FormBuilder no construtor, importar
+  ) {
+    // 3. Iniciar o FormBuilder com os campos do formulário do html
+    this.formGroup = this.formBuilder.group({
+      nome: [''],
+      email: [''],
+      assunto: ['Suporte'],
+      mensagem: ['']
+    });
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FormContatoPage');
   }
 
-  enviarMensagem(){
-    console.log('Mensagem Enviada!');
+  enviarMensagem() {
+    console.log(this.formGroup.value);
   }
 
 }
